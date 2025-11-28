37d7805dda8aaa92dd9258d2e52aba3d165de8f5
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-11-28 06:53:37 +0000

    Add contact form  where user can input and submit their details

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -7,10 +7,44 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
+  const handleChange = (e, setFunc) => {
+    setFunc(e.target.value);
+  };
+
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          value={name}
+          onChange={(e) => handleChange(e, setName)}
+          autoComplete="name"
+        />
+        <label htmlFor="phone">Phone Number</label>
+        <input
+          id="phone"
+          name="phone"
+          type="tel"
+          value={phone}
+          onChange={(e) => handleChange(e, setPhone)}
+          autoComplete="tel"
+        />
+        <label htmlFor="email">E-mail Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e) => handleChange(e, setEmail)}
+          autoComplete="email"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
   );
 };
-

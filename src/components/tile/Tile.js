63f746faa7f42e7916bc17d3ce202ab71a490b81
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-11-29 01:06:51 +0000

    Add tile render component

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ key, name, description }) => {
+  const renderDescription = Object.values(description).map((info, index) => {
+    return <p key={index}>{info}</p>
+  })
   return (
-    <div className="tile-container">
-      
+    <div key={key} className="tile-container">
+      <p className="tile-title">{name}</p>
+      {renderDescription}
     </div>
   );
 };

3632f902372c30b9605be441af749478e0ce26c5
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-11-29 03:16:10 +0000

    Add option to select a saved contact in the appointment form

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ value, setContact, contacts, onChange}) => {
+  const contactPicker = contacts.map((contact) => {
+    return <option value={contact.name}>{contact.name}</option>;
+  });
+
   return (
-    <></>
+    <>
+      <label htmlFor="contact">Contact</label>
+      <select id="contact" name="contact" value={value} onChange={(e) => onChange(e, setContact)}>
+        <option value="">No Contact Selected</option>
+        {contactPicker}
+      </select>
+    </>
   );
 };

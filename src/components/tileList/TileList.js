d7c4b6f4d0662fffe6735a11e6f18a669f8e70d8
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-11-29 01:06:51 +0000

    Add tilelist component

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({ list }) => {
+  const contactsToRender = list?.map((contact, index) => {
+    const { name, ...rest } = contact
+    return <Tile key={index} name={name} description={rest} />
+  })
+
   return (
     <div>
-      
+      {contactsToRender}
     </div>
   );
 };

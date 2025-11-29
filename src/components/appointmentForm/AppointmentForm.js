9c530d8cee6f6bb65f2b1de20fd28489fe9904c5
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-11-29 03:14:34 +0000

    Add appointment form structure and related form functionality

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,9 +1,8 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 const getTodayString = () => {
-  const [month, day, year] = new Date()
-    .toLocaleDateString("en-US")
-    .split("/");
+  const [month, day, year] = new Date().toLocaleDateString("en-US").split("/");
   return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
 };
 
@@ -17,10 +16,43 @@ export const AppointmentForm = ({
   setDate,
   time,
   setTime,
-  handleSubmit
+  handleSubmit,
 }) => {
+  const handleChange = (e, func) => {
+    func(e.target.value);
+  };
 
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          value={title}
+          onChange={(e) => handleChange(e, setTitle)}
+        />
+        <label htmlFor="date">Appointment Date</label>
+        <input
+          id="date"
+          name="date"
+          type="date"
+          min={getTodayString()}
+          value={date}
+          onChange={(e) => handleChange(e, setDate)}
+        />
+        <label htmlFor="time">Appointment Time</label>
+        <input
+          id="time"
+          name="time"
+          type="time"
+          value={time}
+          onChange={(e) => handleChange(e, setTime)}
+        />
+        <ContactPicker value={contact} setContact={setContact} contacts={contacts} onChange={handleChange}  />
+        <button type="submit">Submit</button>
+      </form>
+    </>
   );
 };

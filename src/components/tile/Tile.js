02ce78e54eb0e06d5890c102c86ed7bf6ca5544a
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-11-29 03:09:41 +0000

    Update key value for the component

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export const Tile = ({ key, name, description }) => {
+export const Tile = ({ idx, name, description }) => {
   const renderDescription = Object.values(description).map((info, index) => {
-    return <p key={index}>{info}</p>
+    return <p key={`${info}-${index}`}>{info}</p>
   })
   return (
-    <div key={key} className="tile-container">
+    <div key={idx} className="tile-container">
       <p className="tile-title">{name}</p>
       {renderDescription}
     </div>

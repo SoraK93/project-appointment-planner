201236e45fdb58265bc59e40ac149fe1c7f7a791
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-11-29 01:08:25 +0000

    Add state hooks, reset user input data and render appointmentform and tilelist component

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,27 +3,33 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+export const AppointmentsPage = (props) => {
+  const {contacts, appointments, addAppointment} = props;
+  
+  const [name, setName] = useState()
+  const [contact, setContact] = useState()
+  const [date, setDate] = useState()
+  const [time, setTime] = useState()
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
+    addAppointment(name, contact, date, time)
+    setName("")
+    setContact("")
+    setDate("")
+    setTime("")
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList list={appointments} />
       </section>
     </div>
   );
